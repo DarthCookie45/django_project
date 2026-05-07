9893e86627eec3b6aff46520c3d267c24bee2f3f
Cookie <darthcookienomnom@gmail.com>
2026-05-07 15:55:37 +0100

    Link with Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bp3*k)6r#g4no^x0v_%(za^o333ges3-)l0mb9-eq5ye)%iea)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
